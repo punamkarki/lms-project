09729f16e234fceb93a4e40d4ff3069bff4f8719
punamkarki <karkeepunam25@gmail.com>
2026-08-26 15:17:26 +0545

    change

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="library.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+    <header class="header">
+        <div class="head">
+
+            <a href="index.html" class="brand">
+                <div class="brand-content">
+                    <strong>Smart Library</strong>
+                    <span>Your place to read</span>
+                </div>
+            </a>
+
+            <nav class="main">
+                <a href="index.php" class="menu current"><i class="fa-solid fa-house"></i>Home</a>
+                <a href="books.php" class="menu"><i class="fa-solid fa-book"></i>Books</a>
+                <a href="categories.html" class="menu"><i class="fa-solid fa-layer-group"></i>Categories</a>
+                <a href="about.html" class="menu"><i class="fa-solid fa-circle-info"></i>About</a>
+                <a href="notices.html" class="menu"><i class="fa-solid fa-bell"></i>Notices</a>
+                <a href="contact.html" class="menu"><i class="fa-solid fa-envelope"></i>Contact</a>
+            </nav>
+
+
+            <div class="auth">
+                <a href="login.html" class="signin">Sign in</a>
+
+                <a href="register.html" class="join">
+                    Join Library
+                    <i class="fa-solid fa-arrow-right"></i>
+                </a>
+            </div>
+
+            
+
+        </div>
+    </header>
+</body>
+</html>
--- index.php
+++ index.php
@@ -12,41 +12,9 @@
 </head>
 
 <body>
-
-    <header class="header">
-        <div class="head">
-
-            <a href="index.html" class="brand">
-                <div class="brand-content">
-                    <strong>Smart Library</strong>
-                    <span>Your place to read</span>
-                </div>
-            </a>
-
-            <nav class="main">
-                <a href="index.html" class="menu current"><i class="fa-solid fa-house"></i>Home</a>
-                <a href="books.html" class="menu"><i class="fa-solid fa-book"></i>Books</a>
-                <a href="categories.html" class="menu"><i class="fa-solid fa-layer-group"></i>Categories</a>
-                <a href="about.html" class="menu"><i class="fa-solid fa-circle-info"></i>About</a>
-                <a href="notices.html" class="menu"><i class="fa-solid fa-bell"></i>Notices</a>
-                <a href="contact.html" class="menu"><i class="fa-solid fa-envelope"></i>Contact</a>
-            </nav>
-
-
-            <div class="auth">
-                <a href="login.html" class="signin">Sign in</a>
-
-                <a href="register.html" class="join">
-                    Join Library
-                    <i class="fa-solid fa-arrow-right"></i>
-                </a>
-            </div>
-
-            
-
-        </div>
-    </header>
-
+<?php
+include "header.php"
+?>
     <main>
 
         <section class="hero">
